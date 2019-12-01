7f44ac5a95578e4a6fadf16a84c48d56113d2aa4
Waneksio <waneksiomiszczu@gmail.com>
2019-12-01 21:53:35 +0100

    Add new method expand

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -13,6 +13,16 @@ public class TextTransformer {
 
     public String transform(String text){
         // of course normally it would to something based on transforms
-        return text.toUpperCase();
+        String transformedText = expand(text);
+        return transformedText;
+    }
+
+    public String expand(String text) {
+        text = text.replace("prof.", "profesor").replace("Prof.", "Profesor");
+        text = text.replace("np.", 	"na przykład").replace("Np.", "Na przykład");
+        text = text.replace("dr", "doktor").replace("Dr", "Doktor");
+        text = text.replace("itp.", "i tym podobne").replace("Itp.", "I tym podobne");
+
+        return text;
     }
 }
