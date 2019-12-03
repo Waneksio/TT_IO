881e69132539636143f11bc11b30b5ec5af8770f
Marcin Pastwa <marpas001@gmail.com>
2019-12-03 14:06:02 +0100

    Added WordsToShorts implementation in right place

--- src/main/java/pl/put/poznan/transformer/logic/TextWordsToShorts.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextWordsToShorts.java
@@ -7,10 +7,26 @@ public class TextWordsToShorts extends TextDecorator {
     }
 
     public String transform(String text){
-        // zamiana slow na skroty
+        // Zwijanie skrotow
         // np. na przyklad --> np.
-        // todo
-        // implement
-        return text;
+        String result;
+
+        String textL = text.toLowerCase();
+        text = text.replace("między innymi", "m.in").replace("Między innymi", "M.in").replace("MIĘDZY INNYMI", "M.IN");
+        text = text.replace("na przykład", "np.").replace("Na przykład", "Np.").replace("NA PRZYKŁAD", "NP.");
+        text = text.replace("i tak dalej", "itd.").replace("I tak dalej", "Itd.").replace("I TAK DALEJ", "ITD.");
+        text = text.replace("i tym podobne", "itp.").replace("I tym podobne", "Itp.").replace("I TYM PODOBNE", "ITP.");
+
+        textL = textL.replace("między innymi", "m.in");
+        textL = textL.replace("na przykład", "np.");
+        textL = textL.replace("i tak dalej", "itd.");
+        textL = textL.replace("i tym podobne", "itp.");
+
+        if (textL.length() == text.length()) {
+            result = text;
+        }
+        else {result = textL;}
+
+        return result;
     }
 }
