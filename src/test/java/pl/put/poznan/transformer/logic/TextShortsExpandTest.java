af4bcd773eddd88279699417085c9c498c8e6117
Marcin Pastwa <marpas001@gmail.com>
2020-01-12 12:52:10 +0100

    Add tests using Mockito

--- src/test/java/pl/put/poznan/transformer/logic/TextShortsExpandTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/TextShortsExpandTest.java
@@ -5,12 +5,21 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import static org.mockito.Mockito.*;
+
 public class TextShortsExpandTest {
 
     private TextShortsExpand textShortsExpand;
+    private TextShortsExpand mock;
 
     @Before
-    public void set_up(){ textShortsExpand = new TextShortsExpand(new TextTransform());
+    public void set_up(){
+        textShortsExpand = new TextShortsExpand(new TextTransform());
+        mock = mock(TextShortsExpand.class);
+        when(mock.transform("")).thenReturn("");
+        when(mock.transform("prof.")).thenReturn("profesor");
+        when(mock.transform("dr Ninja")).thenReturn("doktor Ninja");
+
     }
 
     @Test
@@ -20,5 +29,18 @@ public class TextShortsExpandTest {
         assertEquals(textShortsExpand.transform("Pan Prof. spóźnił się na zajęcia."), "Pan Profesor spóźnił się na zajęcia.");
         assertEquals(textShortsExpand.transform("dr Ninja"), "doktor Ninja");
         assertEquals(textShortsExpand.transform("prof., dr itp."), "profesor, doktor i tym podobne");
+
+        // Mock tests
+        String result1 = mock.transform("");
+        verify(mock).transform("");
+        assertEquals(result1, "");
+
+        String result2 = mock.transform("prof.");
+        verify(mock).transform("prof.");
+        assertEquals(result2, "profesor");
+
+        String result3 = mock.transform("dr Ninja");
+        verify(mock).transform("dr Ninja");
+        assertEquals(result3, "doktor Ninja");
     }
 }
--- src/test/java/pl/put/poznan/transformer/logic/TextTransformTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/TextTransformTest.java
@@ -5,13 +5,21 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import static org.mockito.Mockito.*;
+
 public class TextTransformTest {
 
     private TextTransform textTransform;
+    private TextTransform mock;
 
     @Before
     public void set_up(){
         textTransform = new TextTransform();
+        mock = mock(TextTransform.class);
+        when(mock.transform("")).thenReturn("");
+        when(mock.transform("abc")).thenReturn("abc");
+        when(mock.transform("ninja i profesor")).thenReturn("ninja i profesor");
+        when(mock.transform("udana trasformacja")).thenReturn("udana trasformacja");
     }
 
     @Test
@@ -21,6 +29,23 @@ public class TextTransformTest {
         assertEquals(textTransform.transform("abcd"), "abcd");
         assertEquals(textTransform.transform("ninja i zapałki"), "ninja i zapałki");
         assertEquals(textTransform.transform("p12asd"), "p12asd");
+
+        // Mock tests
+        String result1 = mock.transform("");
+        verify(mock).transform("");
+        assertEquals(result1, "");
+
+        String result2 = mock.transform("abc");
+        verify(mock).transform("abc");
+        assertEquals(result2, "abc");
+
+        String result3 = mock.transform("ninja i profesor");
+        verify(mock).transform("ninja i profesor");
+        assertEquals(result3, "ninja i profesor");
+
+        String result4 = mock.transform("udana trasformacja");
+        verify(mock).transform("udana trasformacja");
+        assertEquals(result4,"udana trasformacja");
     }
 
 }
