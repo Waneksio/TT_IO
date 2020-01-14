8a7f773e8342cf0f77a09a91ee7c63bd75118f17
Waneksio <waneksiomiszczu@gmail.com>
2020-01-14 11:02:18 +0100

    Some Changes

--- src/main/java/pl/put/poznan/transformer/logic/Capitalizer.java
+++ src/main/java/pl/put/poznan/transformer/logic/Capitalizer.java
@@ -1,9 +1,24 @@
 package pl.put.poznan.transformer.logic;
 
+/**@author Michasia
+ *
+ * @version 1.0
+ * provides methods for capitalizing words in text
+ */
+
 public class Capitalizer extends TextDecorator{
 
+    /**class constructor
+     *
+     * @param decoratedText the text we will be working with
+     */
     public Capitalizer(TextTransformer decoratedText) { super(decoratedText); }
 
+    /**Capitalizes every single word in the input text
+     *
+     * @param text String of characters to process
+     * @return Transformed text
+     */
     public String transform(String text) {
         String words[] = text.split("\\s");
         String capitalized="";
